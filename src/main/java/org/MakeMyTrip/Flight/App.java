871aeaf6a2217d1b903086395ae7efce6508689c
Abhijit Khuntia <abhijitplaystation@gmail.com>
2025-02-11 00:19:33 +0530

    Adding MakeMyTrip BDD project

---
+++ src/main/java/org/MakeMyTrip/Flight/App.java
@@ -0,0 +1,13 @@
+package org.MakeMyTrip.Flight;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
