1a2ec0a3695e4ee8b1b0569ad3c9dbbe2c132a57
Abhijit Khuntia <abhijitplaystation@gmail.com>
2025-02-19 23:19:04 +0530

    #commit 3

--- src/test/java/org/MakeMyTrip/Flight/StepDefinitions/hooks.java
+++ src/test/java/org/MakeMyTrip/Flight/StepDefinitions/hooks.java
@@ -1,8 +1,12 @@
 package org.MakeMyTrip.Flight.StepDefinitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.MakeMyTrip.Flight.Utils.DependencyResolver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class hooks {
@@ -12,6 +16,14 @@ public class hooks {
     {
         this.globalRes=globalRes;
         driver=this.globalRes.getDriver();
+    }
+
+    @AfterStep
+    public void screenshotTaker(Scenario scenario)
+    {
+        TakesScreenshot taksScreen=(TakesScreenshot) driver;
+        byte[] screenshot= taksScreen.getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot,"image/png",screenshot.toString());
     }
     @After
     public void closer()
--- src/test/java/org/MakeMyTrip/Flight/Suites/RegressionTestSuite.java
+++ src/test/java/org/MakeMyTrip/Flight/Suites/RegressionTestSuite.java
@@ -5,7 +5,9 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/java/org/MakeMyTrip/Flight/Features",
-        glue = "org.MakeMyTrip.Flight.StepDefinitions", monochrome = true)
+        glue = "org.MakeMyTrip.Flight.StepDefinitions", monochrome = true,
+        plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+                "rerun:target/failed_scenarios.txt"})
 class RegressionTestSuite extends AbstractTestNGCucumberTests {
 
     @Override
